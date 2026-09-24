742f89b90f794ef299995597c9cd970299540de3
gomez_d <gomez_d@f213.ub.lan>
2026-09-24 09:49:30 +0800

    feat: adding routing to hoenn component (navbar)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home-component/home-component';
 import { KantoComponent } from './kanto-component/kanto-component';
 import { JohtoComponent } from './johto-component/johto-component';
+import { HoennComponent } from './hoenn-component/hoenn-component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'kanto', component: KantoComponent },
   { path: 'johto', component: JohtoComponent },
+  { path: 'hoenn', component: HoennComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
