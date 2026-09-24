ea07a1ec04e58a37bda911ded0bb5340b4a3dd98
gomez_d <gomez_d@f213.ub.lan>
2026-09-24 09:16:00 +0800

    added hoenn leaders component

---
+++ src/app/hoenn-component/hoenn-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-hoenn-component',
+  styleUrl: './hoenn-component.css',
+  templateUrl: './hoenn-component.html',
+})
+export class HoennComponent {}
