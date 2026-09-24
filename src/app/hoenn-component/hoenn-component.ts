19435532547f585be472b4a7aa2eb3f3947e60e4
gomez_d <gomez_d@f213.ub.lan>
2026-09-24 09:44:56 +0800

    feat: modified hoenn component details and template

--- src/app/hoenn-component/hoenn-component.ts
+++ src/app/hoenn-component/hoenn-component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LeaderInfo } from '../leader-info/leader-info'
+import { HoennLeadersService } from '../hoenn-leaders-service';
 
 @Component({
-  imports: [],
+  imports: [ LeaderInfo ],
+  standalone: true,
   selector: 'app-hoenn-component',
   styleUrl: './hoenn-component.css',
   templateUrl: './hoenn-component.html',
 })
-export class HoennComponent {}
+
+export class HoennComponent {
+  private readonly hoennService = inject(HoennLeadersService);
+
+  hoennLeaders = this.hoennService.hoennLeaders;
+  leaderCount = this.hoennService.leaderCount;
+
+  lastToggledLeader = '';
+
+  onMottoToggled(leaderName: string) {
+    this.lastToggledLeader = leaderName;
+  }
+}
